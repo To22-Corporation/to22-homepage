9f0d01e88b89a6f3aacf64401dfabd83a1c94466
Kohei <mannnnsu446@icloud.com>
2019-01-03 22:14:55 +0900

    add GA

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,3 +92,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+# トラッキングIDを記載
+GA.tracker = "UA-130231521-2"
